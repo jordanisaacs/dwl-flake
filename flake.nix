7701fc2654ba83c1db24b959b53f661d4b6a2e8a
Jordan Isaacs <jordan@snowytrees.dev>
2023-11-25 16:08:14 -0800

    update flake for wlroots 0.17

--- flake.nix
+++ flake.nix
@@ -3,46 +3,63 @@
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
-  outputs = {
-    self,
-    nixpkgs,
-  }: let
-    system = "x86_64-linux";
+  outputs =
+    { self
+    , nixpkgs
+    }:
+    let
+      system = "x86_64-linux";
 
-    pkgs = nixpkgs.legacyPackages.${system};
-
-    dwlBuilder = pkgs.callPackage ./lib {
-      stdenv = pkgs.stdenvAdapters.keepDebugInfo pkgs.stdenv;
-      inherit (pkgs.xorg) xcbutilwm;
-    };
-    dwlPkg = dwlBuilder {config = {};};
-    dwlApp = {
-      type = "app";
-      program = "${dwlPkg}/bin/dwl";
-    };
-  in {
-    apps.${system} = {
-      default = dwlApp;
-      dwl = dwlApp;
-    };
-
-    overlays.default = final: prev: {
-      inherit dwlBuilder;
-      dwlJD = dwlPkg;
-    };
-
-    packages.${system} = {
-      default = dwlPkg;
-      dwl = dwlPkg;
-    };
-
-    devShells.${system}.default = pkgs.mkShell {
-      buildInputs = dwlPkg.buildInputs;
-      nativeBuildInputs =
-        dwlPkg.nativeBuildInputs
-        ++ [
-          pkgs.bear
+      pkgs = nixpkgs.legacyPackages.${system};
+      wlroots = pkgs.wlroots.overrideAttrs (oa: {
+        src = pkgs.fetchFromGitLab {
+          domain = "gitlab.freedesktop.org";
+          owner = "wlroots";
+          repo = "wlroots";
+          rev = "0.17.0";
+          hash = "sha256-VUrnSG4UAAH0cBy15lG0w8RernwegD6lkOdLvWU3a4c=";
+        };
+        buildInputs = with pkgs; oa.buildInputs ++ [
+          hwdata
+          libliftoff
+          libdisplay-info
         ];
+      });
+
+      dwlBuilder = pkgs.callPackage ./lib {
+        stdenv = pkgs.stdenvAdapters.keepDebugInfo pkgs.stdenv;
+        inherit (pkgs.xorg) xcbutilwm;
+        inherit wlroots;
+      };
+      dwlPkg = dwlBuilder { config = { }; };
+      dwlApp = {
+        type = "app";
+        program = "${dwlPkg}/bin/dwl";
+      };
+    in
+    {
+      apps.${system} = {
+        default = dwlApp;
+        dwl = dwlApp;
+      };
+
+      overlays.default = final: prev: {
+        inherit dwlBuilder;
+        dwlJD = dwlPkg;
+      };
+
+      packages.${system} = {
+        default = dwlPkg;
+        dwl = dwlPkg;
+      };
+
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = dwlPkg.buildInputs;
+        nativeBuildInputs =
+          dwlPkg.nativeBuildInputs
+          ++ [
+            pkgs.bear
+          ];
+      };
     };
-  };
 }
--- lib/default.nix
+++ lib/default.nix
@@ -1,30 +1,31 @@
-{
-  lib,
-  stdenv,
-  libinput,
-  libxcb,
-  libxkbcommon,
-  pixman,
-  pkg-config,
-  wayland,
-  wayland-protocols,
-  wlroots_0_16,
-  xwayland,
-  libX11,
-  xcbutilwm,
-}: {config}: let
+{ lib
+, stdenv
+, libinput
+, libxcb
+, libxkbcommon
+, pixman
+, pkg-config
+, wayland
+, wayland-protocols
+, wlroots
+, xwayland
+, libX11
+, xcbutilwm
+}: { config }:
+
+let
   options = lib.evalModules {
     modules = [
-      {imports = [../modules];}
+      { imports = [ ../modules ]; }
       config
     ];
   };
 
   cmdToC = cmd:
     with builtins;
-      if cmd == []
-      then "0"
-      else (concatStringsSep " " (map (x: ''\"'' + x + ''\",'') cmd)) + " NULL";
+    if cmd == [ ]
+    then "0"
+    else (concatStringsSep " " (map (x: ''\"'' + x + ''\",'') cmd)) + " NULL";
   boolToC = bool:
     if bool
     then "1"
@@ -34,72 +35,72 @@
   nscroll = boolToC options.config.input.natscroll;
   cursorsize = builtins.toString options.config.visual.cursorSize;
 in
-  stdenv.mkDerivation {
-    pname = "dwl";
-    version = "unstable";
+stdenv.mkDerivation {
+  pname = "dwl";
+  version = "unstable";
 
-    src = ../.;
+  src = ../.;
 
-    nativeBuildInputs = [pkg-config];
+  nativeBuildInputs = [ pkg-config ];
 
-    buildInputs = [
-      libinput
-      libxcb
-      libxkbcommon
-      pixman
-      wayland
-      wayland-protocols
-      wlroots_0_16
-      libX11
-      xwayland
-      xcbutilwm
-    ];
+  buildInputs = [
+    libinput
+    libxcb
+    libxkbcommon
+    pixman
+    wayland
+    wayland-protocols
+    wlroots
+    libX11
+    xwayland
+    xcbutilwm
+  ];
 
-    patches = [
-      ../patches/fullscreen-rule.patch
-      ../patches/module.patch
-      ../patches/module_naturalscroll.patch
-      ../patches/module_cursor.patch
-    ];
+  patches = [
+    ../patches/fullscreen-rule.patch
+    ../patches/module.patch
+    ../patches/module_naturalscroll.patch
+    ../patches/module_cursor.patch
+  ];
 
-    postPatch = ''
-      substituteInPlace ./config.def.h --replace "@termcmd@" "${cmds.term}"
-      substituteInPlace ./config.def.h --replace "@menucmd@" "${cmds.menu}"
-      substituteInPlace ./config.def.h --replace "@quitcmd@" "${cmds.quit}"
-      substituteInPlace ./config.def.h --replace "@audioupcmd@" "${cmds.audioup}"
-      substituteInPlace ./config.def.h --replace "@audiomutcmd@" "${cmds.audiomut}"
-      substituteInPlace ./config.def.h --replace "@audiofrwcmd@" "${cmds.audiofrw}"
-      substituteInPlace ./config.def.h --replace "@audioprevcmd@" "${cmds.audioprev}"
-      substituteInPlace ./config.def.h --replace "@audiodowncmd@" "${cmds.audiodown}"
-      substituteInPlace ./config.def.h --replace "@audioplaycmd@" "${cmds.audioplay}"
-      substituteInPlace ./config.def.h --replace "@audioplaycmd@" "${cmds.audioplay}"
-      substituteInPlace ./config.def.h --replace "@naturalscroll@" "${nscroll}"
-      substituteInPlace ./dwl.c --replace "@cursorsize@" "${cursorsize}"
+  postPatch = ''
+    substituteInPlace ./config.def.h --replace "@termcmd@" "${cmds.term}"
+    substituteInPlace ./config.def.h --replace "@menucmd@" "${cmds.menu}"
+    substituteInPlace ./config.def.h --replace "@quitcmd@" "${cmds.quit}"
+    substituteInPlace ./config.def.h --replace "@audioupcmd@" "${cmds.audioup}"
+    substituteInPlace ./config.def.h --replace "@audiomutcmd@" "${cmds.audiomut}"
+    substituteInPlace ./config.def.h --replace "@audiofrwcmd@" "${cmds.audiofrw}"
+    substituteInPlace ./config.def.h --replace "@audioprevcmd@" "${cmds.audioprev}"
+    substituteInPlace ./config.def.h --replace "@audiodowncmd@" "${cmds.audiodown}"
+    substituteInPlace ./config.def.h --replace "@audioplaycmd@" "${cmds.audioplay}"
+    substituteInPlace ./config.def.h --replace "@audioplaycmd@" "${cmds.audioplay}"
+    substituteInPlace ./config.def.h --replace "@naturalscroll@" "${nscroll}"
+    substituteInPlace ./dwl.c --replace "@cursorsize@" "${cursorsize}"
+  '';
+
+  dontConfigure = true;
+
+  installPhase = ''
+    runHook preInstall
+    make install DESTDIR=$out PREFIX=
+    runHook postInstall
+  '';
+
+  meta = with lib; {
+    homepage = "https://github.com/djpohly/dwl/";
+    description = "Dynamic window manager for Wayland";
+    longDescription = ''
+      dwl is a compact, hackable compositor for Wayland based on wlroots. It is
+      intended to fill the same space in the Wayland world that dwm does in X11,
+      primarily in terms of philosophy, and secondarily in terms of
+      functionality. Like dwm, dwl is:
+      - Easy to understand, hack on, and extend with patches
+      - One C source file (or a very small number) configurable via config.h
+      - Limited to 2000 SLOC to promote hackability
+      - Tied to as few external dependencies as possible
     '';
-
-    dontConfigure = true;
-
-    installPhase = ''
-      runHook preInstall
-      make install DESTDIR=$out PREFIX=
-      runHook postInstall
-    '';
-
-    meta = with lib; {
-      homepage = "https://github.com/djpohly/dwl/";
-      description = "Dynamic window manager for Wayland";
-      longDescription = ''
-        dwl is a compact, hackable compositor for Wayland based on wlroots. It is
-        intended to fill the same space in the Wayland world that dwm does in X11,
-        primarily in terms of philosophy, and secondarily in terms of
-        functionality. Like dwm, dwl is:
-        - Easy to understand, hack on, and extend with patches
-        - One C source file (or a very small number) configurable via config.h
-        - Limited to 2000 SLOC to promote hackability
-        - Tied to as few external dependencies as possible
-      '';
-      license = licenses.gpl3Only;
-      maintainers = with maintainers; [AndersonTorres];
-      inherit (wayland.meta) platforms;
-    };
-  }
+    license = licenses.gpl3Only;
+    maintainers = with maintainers; [ AndersonTorres ];
+    inherit (wayland.meta) platforms;
+  };
+}
